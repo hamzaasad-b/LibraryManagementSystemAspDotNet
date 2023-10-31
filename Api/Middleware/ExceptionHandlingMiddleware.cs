df7ba92d86d78261c86c64bae13b46be26331ab1
Hamza Asad <hamzaasad@folio3.com>
2023-10-31 19:47:15 +0500

    refactor: project naming update

---
+++ Api/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,31 @@
+﻿namespace Api.Middleware
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                // Handle the exception and generate an appropriate response.
+                // You can log the exception, customize the error response, etc.
+                context.Response.StatusCode = 500; // Internal Server Error
+                context.Response.ContentType = "application/json";
+
+                await context.Response.WriteAsync($"An error occurred: {ex.Message}");
+            }
+        }
+
+
+    }
+}
