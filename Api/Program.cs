cf4606197f3324408130a5586a7523f8feb08909
Hamza Asad <hamzaasad@folio3.com>
2023-11-01 17:02:40 +0500

    feat: entities added

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,8 +1,11 @@
 using Api.Middleware;
+using Data.Context;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<LmsDbContext>( options => options.UseSqlite("DataSource=app.db") );
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- Data/Context/LmsDbContext.cs
+++ Data/Context/LmsDbContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Data.Entities;
+using Data.Entities.Map;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -12,13 +13,17 @@ namespace Data.Context
 {
     public class LmsDbContext : IdentityDbContext<User, IdentityRole<uint>, uint>
     {
-        public LmsDbContext(DbContextOptions<LmsDbContext> options)
+        private DbSet<Book>? Books { get; set; }
+
+        public LmsDbContext(DbContextOptions<LmsDbContext> options) : base(options)
         {
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            new UserMap().Configure(modelBuilder.Entity<User>());
+            new BookMap().Configure(modelBuilder.Entity<Book>());
         }
     }
 }
