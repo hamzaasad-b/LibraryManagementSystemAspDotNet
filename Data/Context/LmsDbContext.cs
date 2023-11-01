0a50b5937f02716aa75a1169031f055aefa73d59
Hamza Asad <hamzaasad@folio3.com>
2023-11-01 15:23:10 +0500

    feat: added identity models

--- Data/Context/LmsDbContext.cs
+++ Data/Context/LmsDbContext.cs
@@ -3,21 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Data.Context
 {
-    public class LmsDbContext : DbContext
+    public class LmsDbContext : IdentityDbContext<User, IdentityRole<uint>, uint>
     {
         public LmsDbContext(DbContextOptions<LmsDbContext> options)
         {
-
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
         }
-
     }
 }
